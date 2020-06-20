14b163bac0f89ce20c50b0fceb58aa9570b3ef2d
MrVoltz <uzivatelmp@gmail.com>
2020-06-20 15:46:42 +0200

    Properly disable certificate verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+rcfile=$(mktemp)
+echo>$rcfile
+
+echo "set ftp:ssl-allow false">>$rcfile
+echo "set ftp:ssl-force $INPUT_FORCESSL">>$rcfile
+echo "set ssl:verify-certificate no">>$rcfile
+echo "set sftp:auto-confirm yes">>$rcfile
+echo "set ftp:ssl-protect-data yes">>$rcfile
+echo "set xfer:timeout 5s">>$rcfile
+echo "set net:timeout 5s">>$rcfile
+
+lftp --rcfile=$rcfile $INPUT_HOST -u "$INPUT_USER,$INPUT_PASSWORD" -e "mirror -R -P 4 -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+rm -f $rcfile
